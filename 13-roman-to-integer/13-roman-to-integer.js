55dfaca4a7c35d3877448836bed6ac04d36fce2d
Farouk Amr <50875890+FaroukAmr@users.noreply.github.com>
2022-06-03 22:52:42 +0200

    Time: 255 ms (16.32%), Space: 47 MB (73.48%) - LeetHub

---
+++ 13-roman-to-integer/13-roman-to-integer.js
@@ -0,0 +1,34 @@
+let symbols = ["I","V","X","L","C","D","M"];
+let value = [1,5,10,50,100,500,1000];
+var romanToInt = function(s) { 
+    let sum=0;
+    for(let i=0;i<s.length;i++){
+        sum+=findIndex(s.charAt(i),s.charAt(i+1)); 
+    }
+    return sum;
+
+};
+
+function findIndex(s,n){
+    
+
+    if(s==="I" && (n==="V" || n=="X")){
+        return -1;
+    }
+
+    if(s==="X" && (n==="L" || n=="C")){
+        return -10;
+    }
+
+    if(s==="C" && (n==="D" || n=="M")){
+        return -100;
+    }
+
+    for(let i=0;i<symbols.length;i++){
+
+        if(s===symbols[i]){
+            return value[i];
+        }
+    }
+    return -1;
+}
