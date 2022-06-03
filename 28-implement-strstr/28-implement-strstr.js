f125e1009bfdc5fdaefc66b940707d3da31bfb00
Farouk Amr <50875890+FaroukAmr@users.noreply.github.com>
2022-06-03 23:59:19 +0200

    Time: 114 ms (8.25%), Space: 43 MB (8.15%) - LeetHub

--- 28-implement-strstr/28-implement-strstr.js
+++ 28-implement-strstr/28-implement-strstr.js
@@ -1,22 +1,20 @@
 var strStr = function(haystack, needle) {
     let total=0;
-    let needleLength= needle.length
-    let stackLength=haystack.length
     let index=-1;
     if(needle===""){
         return 0;
     }
 
-    if(needleLength>stackLength){
+    if(needle.length>haystack.length){
         return -1;
     }
     if(haystack===needle){
         return 0;
     }
 
-    for(let i=0;i<stackLength;i++){
+    for(let i=0;i<haystack.length;i++){
        if(haystack.charAt(i)===needle.charAt(0)){
-        for(let j=0;j<needleLength;j++){
+        for(let j=0;j<needle.length;j++){
             if(haystack.charAt(i+j)===needle.charAt(j)){
                 
                 total++
@@ -25,7 +23,7 @@ var strStr = function(haystack, needle) {
                 continue
             }
         }
-        if(total===needleLength){
+        if(total===needle.length){
             return i;
         }
        }
