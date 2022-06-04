c58ba6606bd6626b2649a2b062d8c9d8c5eaeea4
Farouk Amr <50875890+FaroukAmr@users.noreply.github.com>
2022-06-04 23:04:35 +0200

    Time: 104 ms (13.17%), Space: 42.2 MB (34.89%) - LeetHub

--- 58-length-of-last-word/58-length-of-last-word.js
+++ 58-length-of-last-word/58-length-of-last-word.js
@@ -1,6 +1,5 @@
 var lengthOfLastWord = function(s) {
  let myArray = s.split(" ")
- console.log(myArray)
  for(let i=myArray.length-1; i>=0;i--){
     if(myArray[i]!=""){
         return myArray[i].length;
