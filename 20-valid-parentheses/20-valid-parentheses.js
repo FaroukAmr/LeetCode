51bbfe6e89ede2b0cb4de9f13f214dee3465333d
Farouk Amr <50875890+FaroukAmr@users.noreply.github.com>
2022-06-05 12:18:35 +0200

    Time: 92 ms (36.60%), Space: 41.9 MB (89.41%) - LeetHub

---
+++ 20-valid-parentheses/20-valid-parentheses.js
@@ -0,0 +1,28 @@
+var isValid = function(s) {
+   let stack =[]
+   for(let i=0;i<s.length;i++){
+       switch(s.charAt(i)){
+           case ")":
+               if(stack.pop()!="("){
+                   return false;
+               }
+               break;
+            case "]":
+                if(stack.pop()!="["){
+                    return false;
+                }
+                break;
+            case "}":
+                if(stack.pop()!="{"){
+                    return false;
+                }
+                break;
+            default:
+                stack.push(s.charAt(i))
+       }
+   }
+   if(stack.length==0){
+       return true
+   }
+   return false;
+}
