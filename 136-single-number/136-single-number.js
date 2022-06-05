44344b7df03935e50e6f00f6325529f5b8f5c814
Farouk Amr <50875890+FaroukAmr@users.noreply.github.com>
2022-06-06 00:29:46 +0200

    Time: 335 ms (12.68%), Space: 46.7 MB (39.62%) - LeetHub

--- 136-single-number/136-single-number.js
+++ 136-single-number/136-single-number.js
@@ -2,11 +2,11 @@ var singleNumber = function(nums) {
     let stack=[]
     let index=-1;
     for(let i=0;i<nums.length;i++){
-        if(stack.includes(nums[i])){
-            index=stack.indexOf(nums[i])
-            stack.splice(index,1)
+        if(stack.indexOf(nums[i])!=-1){
+            index=stack.indexOf(nums[i]);
+            stack.splice(index,1);
         }else{
-            stack.push(nums[i])
+            stack.push(nums[i]);
         }
     }
     return stack
