056bc90e2ff70b7bd088da562c2100a29241978d
Farouk Amr <50875890+FaroukAmr@users.noreply.github.com>
2022-06-06 10:50:33 +0200

    Time: 88 ms (68.26%), Space: 44.9 MB (60.62%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.js
+++ 125-valid-palindrome/125-valid-palindrome.js
@@ -1,12 +1,7 @@
 var isPalindrome = function(s) {
-    let c=""
-    for(let i=0; i<s.length;i++){
-        if(s.charAt(i).toLowerCase()!=s.charAt(i).toUpperCase()|| /^\d$/.test(s.charAt(i))){
-            c+=s.charAt(i).toLowerCase();
-        }
-    }
-    for(let i=0;i<c.length/2;i++){
-        if(c.charAt(i)!=c.charAt(c.length-1-i)){
+    s = s.replace(/[^0-9A-Z]+/gi, "").toLowerCase()
+    for(let i=0;i<s.length/2;i++){
+        if(s.charAt(i)!=s.charAt(s.length-1-i)){
             return false;
         }
     }
