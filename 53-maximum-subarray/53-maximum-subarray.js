cb6fcec2bef588c4bf451466c1c24d2acf476324
Farouk Amr <50875890+FaroukAmr@users.noreply.github.com>
2022-06-07 17:03:01 +0200

    Time: 74 ms (98.66%), Space: 50.4 MB (58.32%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.js
+++ 53-maximum-subarray/53-maximum-subarray.js
@@ -2,11 +2,7 @@ var maxSubArray = function(nums) {
     let max=nums[0]
     let currentmax=nums[0]
     for(let i=1;i<nums.length;i++){
-        if(currentmax+nums[i]>nums[i]){
-            currentmax+=nums[i]
-        }else{
-            currentmax=nums[i]
-        }
+        currentmax=currentmax+nums[i]>nums[i]?currentmax+=nums[i]:nums[i]
 
         if(max<currentmax){
             max=currentmax
