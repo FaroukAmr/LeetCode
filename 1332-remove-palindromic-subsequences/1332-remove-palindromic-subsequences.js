4ab9b4a56ef3e27db0a8d5366a98045ed488170e
Farouk Amr <50875890+FaroukAmr@users.noreply.github.com>
2022-06-09 01:34:32 +0200

    Time: 69 ms (79.79%), Space: 42 MB (44.68%) - LeetHub

--- 1332-remove-palindromic-subsequences/1332-remove-palindromic-subsequences.js
+++ 1332-remove-palindromic-subsequences/1332-remove-palindromic-subsequences.js
@@ -1,8 +1,8 @@
 var removePalindromeSub = function(s) {
     for(let i=0;i<s.length/2;i++){
         if(s.charAt(i)!=s.charAt(s.length-1-i)){
-            return 2;
+            return 2
         }
     }
-    return 1;
+    return 1
 }
