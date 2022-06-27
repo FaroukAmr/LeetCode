aaf8642ff465b57c44bde01c5737963da7e61e7b
Farouk Amr <50875890+FaroukAmr@users.noreply.github.com>
2022-06-28 00:32:33 +0200

    Time: 74 ms (97.44%), Space: 50.3 MB (51.76%) - LeetHub

--- 1689-partitioning-into-minimum-number-of-deci-binary-numbers/1689-partitioning-into-minimum-number-of-deci-binary-numbers.js
+++ 1689-partitioning-into-minimum-number-of-deci-binary-numbers/1689-partitioning-into-minimum-number-of-deci-binary-numbers.js
@@ -1,13 +1,4 @@
 var minPartitions = function(n) {
-   let max=0;
-   for(let i=0; i<n.length;i++){
-      if(n.charAt(i)==9){
-         return 9;
-      }else{
-         if(n.charAt(i)>max){
-            max=n.charAt(i);
-         }
-      }
-   }
-   return max;
+   let val= n.split("")
+   return Math.max.apply(Math, val);
 };
